019b11931e4f63eebbe774ad29efca6df6cb8a6d
Abuthawabeh <36339960+Abuthawabeh@users.noreply.github.com>
2018-02-10 19:31:50 +0000

    Add files via upload

---
+++ src/student/Short_question.java
@@ -0,0 +1,219 @@
+
+import java.sql.*;
+ import java.awt.*;
+ import java.awt.event.*;
+ import javax.swing.*;
+ import java.util.*;
+ import java.text.*;
+
+
+ public class Short_question extends JFrame {
+
+
+          private javax.swing.JButton plainButton;
+          private JComboBox chooseExam;
+
+          ArrayList tableName;
+          ArrayList e ;
+          String Exam_name="exam";
+          JTextField arrayText[ ];
+          private Container c;
+          private FlowLayout layout;
+
+     public Short_question() {
+super("«”∆·… «·«„ Õ«‰«  ");
+
+//////
+
+ try {
+				  Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+				  // connect to db using DriverManager
+				  Connection conn =DriverManager.getConnection( "jdbc:odbc:question" );
+		          DatabaseMetaData metadata =conn.getMetaData();
+		String [] tableTypes = { "TABLE" };
+		ResultSet tables =metadata.getTables(null, null, null, tableTypes );
+
+				  // Create a statement object
+		          //Statement statement =	conn.createStatement();
+
+		tableName=new ArrayList();
+		int i=0;
+		while( tables.next() )
+		{tableName.add(tables.getString("TABLE_NAME"));
+		++i;
+
+		}
+
+		             conn.close();
+		            }
+					   catch ( ClassNotFoundException cnfex ){}
+					   catch ( SQLException sqlex )
+				  	{}
+
+
+
+
+
+/////
+
+
+            // Object[] possibleValues = { "First", "Second", "Third" };
+			 Exam_name= (String)JOptionPane.showInputDialog(null,
+			 "Choose one", "Input",
+			 JOptionPane.INFORMATION_MESSAGE, null,
+			tableName.toArray(), tableName.get(0));
+
+
+
+
+             plainButton = new javax.swing.JButton("«·‰ ÌÃ…");
+
+                    layout=new FlowLayout();
+			 		c=getContentPane();
+			 		c.setLayout(layout);
+
+
+
+             try {
+			 		  Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+			 		  // connect to db using DriverManager
+			 		  Connection conn =DriverManager.getConnection( "jdbc:odbc:question" );
+
+			 		  // Create a statement object
+			         Statement statement =	conn.createStatement();
+
+			         ResultSet rs1=statement.executeQuery("select DISTINCT g from "+Exam_name);
+
+                  e=new ArrayList();
+
+			        JLabel array[ ]=new JLabel[10];
+			        arrayText=new JTextField[10];
+					 int i=0;
+					 while(rs1.next())
+					 {array[i]=new JLabel();
+
+					  array[i].setText((i+1)+"  "+rs1.getString("g")+"\n\n\n");
+
+                      getContentPane().add(array[i]);
+                      arrayText[ i]=new  JTextField(20);
+                      getContentPane().add(arrayText[ i]);
+                      ++i;
+                      }
+                   ResultSet   rs3=statement.executeQuery("select DISTINCT g from "+Exam_name);
+                     //rs1.beforeFirst();
+                     i=0;
+                     while(rs3.next())
+                      {e.add(rs3.getString("g"));
+                      ++i;}
+
+
+
+             statement.close();
+             conn.close();}
+			 			   catch ( ClassNotFoundException cnfex ){}
+			 			   catch ( SQLException sqlex )
+		  	{}
+
+
+
+
+
+
+     plainButton.addActionListener(new java.awt.event.ActionListener() {
+         public void actionPerformed(java.awt.event.ActionEvent evt) {
+
+try {
+						 		  Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+						 		  // connect to db using DriverManager
+						 		  Connection conn1 =DriverManager.getConnection( "jdbc:odbc:question" );
+
+						 		  // Create a statement object
+						         Statement statement1 =	conn1.createStatement();
+
+			                    ResultSet rs2=statement1.executeQuery("select  g,m,x from "+Exam_name);
+            int y,j;
+			int mark=0;
+			int total=0;
+
+            StringTokenizer tokens;
+
+String n,r,w,u="";
+
+
+                 for(int i=0;i<e.size();++i)
+                 {
+
+
+
+                   while(rs2.next()) {
+                      n=rs2.getString("g");
+                      if(n.equals(e.get(i)))
+                       {
+
+
+					 	                tokens=new StringTokenizer(arrayText[i].getText());
+									     j= rs2.getInt("x");
+									     total+=j;
+
+                                         r=rs2.getString("m");
+
+										 while(tokens.hasMoreTokens())
+										 {w=tokens.nextToken();
+
+											 if(w.equals(r)&&(!u.contains(w)))
+										     { mark+=j;u+=w; }
+				                         }
+                                         u="";
+
+                        }
+
+	    	         }
+                     rs2=statement1.executeQuery("select  g,m,x from "+Exam_name);
+                 }
+          JOptionPane.showMessageDialog(null,"Result= "+mark+"\n From "+total);
+
+                       statement1.close();
+		               conn1.close();}
+		  			 			   catch ( ClassNotFoundException cnfex ){}
+		  			 			   catch ( SQLException sqlex )
+		  		  	{}
+
+
+
+
+
+
+
+
+          }
+        });
+
+
+
+
+
+
+        ////////////////
+
+
+
+      getContentPane().add(plainButton);
+
+
+
+      setSize(250, 350);
+
+
+         }
+
+
+
+
+    public static void main(String args[]) {
+	        new Short_question().show();
+    }
+
+
+}
+
+
